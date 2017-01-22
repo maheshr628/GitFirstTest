fffbede2f09ef0e5958fe43a6a0c4a3e293c61be
maheshr628 <maheshr628@gmail.com>
2017-01-22 19:16:00 +0530

    Initial Commit

---
+++ FirstGitTest/src/com/mahesh/main/TestMain.java
@@ -0,0 +1,16 @@
+package com.mahesh.main;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestSalesData tsd = new TestSalesData();
+		tsd.displaySales();
+		doDisplay();
+	}
+	
+	public static void doDisplay(){
+		System.out.println("Hello Git!!!!!!!!");
+	}
+
+}
---
+++ FirstGitTest/src/com/mahesh/main/TestSalesData.java
@@ -0,0 +1,9 @@
+package com.mahesh.main;
+
+public class TestSalesData {
+	
+	public void displaySales(){
+		System.out.println("You r in TestSalesData!!!!");
+	}
+
+}
